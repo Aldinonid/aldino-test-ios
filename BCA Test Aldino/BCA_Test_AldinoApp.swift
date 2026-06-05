29738443fbe1cd2c884339426770e89e70cf1581
Aldinonid <aldino.efendi@icloud.com>
2026-06-06 06:42:07 +0700

    Initial Commit

---
+++ BCA Test Aldino/BCA_Test_AldinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BCA_Test_AldinoApp.swift
+//  BCA Test Aldino
+//
+//  Created by Aldino Efendi on 2026/06/06.
+//
+
+import SwiftUI
+
+@main
+struct BCA_Test_AldinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
